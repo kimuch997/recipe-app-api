d910867c3fffbac15142cbac77f606bf0747d61a
kimuch997 <lxanthonykim@gmail.com>
2025-05-16 11:33:42 +0300

    Configured docker and checks to wait_for_db

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,9 @@
-"""
-Calculator
-"""
-def add(x,y):
-    return x + y
+# """
+# Calculator
+# """
+# def add(x,y):
+#     return x + y
 
-def subtract(x,y):
-    """ Subtract x from y and return results """
-    return x - y
+# def subtract(x,y):
+#     """ Subtract x from y and return results """
+#     return x - y
