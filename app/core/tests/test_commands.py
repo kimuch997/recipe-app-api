0312f6e8070cd792e60f479a76a087668d346959
kimuch997 <lxanthonykim@gmail.com>
2025-05-16 12:00:47 +0300

    Configured docker and checks to wait_for_db4

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -8,9 +8,9 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """ Test Commands"""
+    @patch('core.management.commands.wait_for_db.Command.check')
     def test_wait_for_db_ready(self, patched_check):
         """ Test waiting for database if database ready."""
         patched_check.return_value = True
@@ -18,7 +18,8 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
-
+        
+    @patch('core.management.commands.wait_for_db.Command.check')
     @patch('time.sleep')
     def test_wait_for_db_delay(self ,patched_sleep):
         """ Tests waiting for database when getting Operational Error"""
